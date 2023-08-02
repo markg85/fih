d214fce93b69de08397d44b49644632dabd65a6c
Mark Gaiser <markg85@gmail.com>
2023-08-02 20:54:45 +0200

    Rebuild in a super simple version. No more metadata bookkeeping at all. We'll see how this works.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function isEmptyObject(obj) {
     return Object.keys(obj).length === 0;
 }
 
-const imageExistsByHash = async (hash) => {
+const imageExists = async (hash) => {
     try {
         await fs.access(path.join(IMAGEFOLDER, hash))
         return true;
@@ -24,41 +24,44 @@ const imageExistsByHash = async (hash) => {
     }
 }
 
-const touch = async (path) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            await fs.access(path, fs.constants.R_OK | fs.constants.W_OK)
-            resolve()
-            return;
-        } catch (error) {
-            // nothing. Not an "error" error...
-        }
+// const touch = async (path) => {
+//     console.log(path)
+//     return new Promise(async (resolve, reject) => {
+//         try {
+//             await fs.access(path, fs.constants.R_OK | fs.constants.W_OK)
+//             resolve()
+//             return;
+//         } catch (error) {
+//             // nothing. Not an "error" error...
+//         }
         
-        await fs.writeFile(path, "{}")
-        resolve()
-        return;
-    });
-  };
+//         await fs.writeFile(path, "{}")
+//         resolve()
+//         return;
+//     });
+//   };
 
-const metadataFile = async (hash) => {
-    try {
-        const filePath = path.join(METADATAFOLDER, `${hash}.json`)
-        await touch(filePath);
-        const data = await fs.readFile(filePath)
-        let jsonData = JSON.parse(data)
-        if (typeof jsonData == 'object') {
-            return jsonData;
-        }
-    } catch (error) {
-        console.log(error)
-        return {};
-    }
-    return {}
-}
+// const metadataFile = async (hash) => {
+//     try {
+//         metadatafilerwlock.add(hash);
+//         const filePath = path.join(METADATAFOLDER, `${hash}.json`)
+//         await touch(filePath);
+//         const data = await fs.readFile(filePath)
+//         let jsonData = JSON.parse(data.toString())
+//         if (typeof jsonData == 'object') {
+//             return jsonData;
+//         }
+//     } catch (error) {
+//         // console.log(error)
+//         throw new Error(`Killed metadataFile function, someone else is still reading/writing.`);
+//     } finally {
+//         return {}
+//     }
+// }
 
-const saveMetadata = async (metadata) => {
-    return fs.writeFile(path.join(METADATAFOLDER, `${metadata.source.hash}.json`), JSON.stringify(metadata))
-}
+// const saveMetadata = async (metadata) => {
+//     return fs.writeFile(path.join(METADATAFOLDER, `${metadata.source.hash}.json`), JSON.stringify(metadata))
+// }
 
 const downloadFile = async (url, hash) => {
     let file = null;
@@ -90,100 +93,98 @@ const downloadFile = async (url, hash) => {
     }
 }
 
-const addVariant = (metadata, imageMetadataObject, hash, extension, filename) => {
-    const newVariant = {
-        cid: 'TODO: CID',
-        hash: hash,
-        extension: extension,
-        filename: filename,
-        width: imageMetadataObject.width,
-        height: imageMetadataObject.height
-    }
+// const addVariant = (metadata, imageMetadataObject, hash, extension, filename) => {
+//     const newVariant = {
+//         cid: 'TODO: CID',
+//         hash: hash,
+//         extension: extension,
+//         filename: filename,
+//         width: imageMetadataObject.width,
+//         height: imageMetadataObject.height
+//     }
 
-    metadata.variants.push(newVariant)
-    return newVariant
-}
+//     metadata.variants.push(newVariant)
+//     return newVariant
+// }
 
-const flattenedVariants = (metadata) => {
-    return [metadata.source, ...metadata.variants];
-}
+// const flattenedVariants = (metadata) => {
+//     return [metadata.source, ...metadata.variants];
+// }
 
-const variantFromTallestWithoutGrace = (tallestRequest, extension, variants) => {
-    const result = variants.filter(variant => Math.max(variant.width, variant.height) == tallestRequest && variant.extension == extension);
-    if (result == []) {
-        return false;
-    }
-    return result[0];
-}
+// const variantFromTallestWithoutGrace = (tallestRequest, extension, variants) => {
+//     const result = variants.filter(variant => Math.max(variant.width, variant.height) == tallestRequest && variant.extension == extension);
+//     if (result == []) {
+//         return false;
+//     }
+//     return result[0];
+// }
 
-const getExistingVariant = (metadata, options) => {
-    const variants = flattenedVariants(metadata)
-    // console.log(variants)
-    // console.log(options)
+// const getExistingVariant = (metadata, options) => {
+//     const variants = flattenedVariants(metadata)
+//     // console.log(variants)
+//     // console.log(options)
 
-    if (options?.tallestSide) {
-        if (options?.gracePercentage) {
-            return variantFromTallestWithoutGrace(options.tallestSide, options.extension, variants)
-        } else {
-            return variantFromTallestWithoutGrace(options.tallestSide, options.extension, variants)
-        }
-    }
+//     if (options?.tallestSide) {
+//         if (options?.gracePercentage) {
+//             return variantFromTallestWithoutGrace(options.tallestSide, options.extension, variants)
+//         } else {
+//             return variantFromTallestWithoutGrace(options.tallestSide, options.extension, variants)
+//         }
+//     }
 
-    return metadata.source;
-}
+//     return metadata.source;
+// }
 
-const handleProcessing = async (metadata, options, sourceImageSharp) => {
-    const existingVariant = getExistingVariant(metadata, options)
+// const handleProcessing = async (metadata, options, sourceImageSharp) => {
+//     const existingVariant = getExistingVariant(metadata, options)
     
-    if (existingVariant) {
-        console.log(`Requested image matched existing images. Returned hash: ${existingVariant.hash} (width: ${existingVariant.width}, height: ${existingVariant.height})`)
-        return {hash: existingVariant.hash, filename: existingVariant.filename}
-    } else {
-        const hash = metadata.source.hash;
+//     if (existingVariant) {
+//         console.log(`Requested image matched existing images. Returned hash: ${existingVariant.hash} (width: ${existingVariant.width}, height: ${existingVariant.height})`)
+//         return {hash: existingVariant.hash, filename: existingVariant.filename}
+//     } else {
+//         const hash = metadata.source.hash;
 
-        if (options?.tallestSide) {
-            if (sourceImageSharp == null) {
-                sourceImageSharp = sharp(path.join(IMAGEFOLDER, hash));
-            }
+//         if (options?.tallestSide) {
+//             if (sourceImageSharp == null) {
+//                 sourceImageSharp = sharp(path.join(IMAGEFOLDER, hash));
+//             }
 
-            const sourceImageMetadata = await sourceImageSharp.metadata();
-            let resizeOptions = {}
-            if (sourceImageMetadata.width > sourceImageMetadata.height) {
-                resizeOptions.width = options.tallestSide;
-            } else {
-                resizeOptions.height = options.tallestSide;
-            }
+//             const sourceImageMetadata = await sourceImageSharp.metadata();
+//             let resizeOptions = {}
+//             if (sourceImageMetadata.width > sourceImageMetadata.height) {
+//                 resizeOptions.width = options.tallestSide;
+//             } else {
+//                 resizeOptions.height = options.tallestSide;
+//             }
     
-            const newImageBuffer = await sourceImageSharp.resize(resizeOptions).toBuffer()
-            const resizedHash = Buffer.from(blake3(newImageBuffer)).toString('hex')
+//             const newImageBuffer = await sourceImageSharp.resize(resizeOptions).toBuffer()
+//             const resizedHash = Buffer.from(blake3(newImageBuffer)).toString('hex')
     
-            try {
-                const destImage = sharp(newImageBuffer);
-                filename = `${resizedHash}.${options.extension}`
+//             try {
+//                 const destImage = sharp(newImageBuffer);
+//                 filename = `${resizedHash}.${options.extension}`
 
-                if (options.extension == 'avif') {
-                    await destImage.avif({ quality: 75 }).toFile(path.join(IMAGEFOLDER, filename))
-                } else if (options.extension == 'heif') {
-                    await destImage.heif({ quality: 75, compression: 'hevc' }).toFile(path.join(IMAGEFOLDER, filename))
-                }
+//                 if (options.extension == 'avif') {
+//                     await destImage.avif({ quality: 75 }).toFile(path.join(IMAGEFOLDER, filename))
+//                 } else if (options.extension == 'heif') {
+//                     await destImage.heif({ quality: 75, compression: 'hevc' }).toFile(path.join(IMAGEFOLDER, filename))
+//                 }
 
-                const newVariant = addVariant(metadata, await destImage.metadata(), resizedHash, options.extension, filename)
-                await saveMetadata(metadata);
-                console.log(`Created new variant image from hash ${hash}. New width: ${newVariant.width} and height: ${newVariant.height}`)
+//                 const newVariant = addVariant(metadata, await destImage.metadata(), resizedHash, options.extension, filename)
+//                 await saveMetadata(metadata);
+//                 console.log(`Created new variant image from hash ${hash}. New width: ${newVariant.width} and height: ${newVariant.height}`)
 
-                return {hash: newVariant.hash, filename: newVariant.filename}
-            } catch (error) {
-                console.log(error)
-                return null;
-            }
-        }
-        return null;
-    }
-}
+//                 return {hash: newVariant.hash, filename: newVariant.filename}
+//             } catch (error) {
+//                 console.log(error)
+//                 return null;
+//             }
+//         }
+//         return null;
+//     }
+// }
 
 fastify.post('/*', async (request, reply) => {
-
-    let options = {}
 
     try {
         /**
@@ -200,59 +201,53 @@ fastify.post('/*', async (request, reply) => {
          *    TODO        - token <access token>                          // The token allowing you access to request images through this method and thereby downloading them on the server too.
          * }
          */
-    
-        console.log(request.body)
-    
-        let metadata = null
-        let sourceImageSharp = null
 
-        // Handle the requested options.
-        options = request.body;
+        // // Handle the requested options.
+        let options = request.body;
 
         if (!options?.extension) {
             options.extension = 'avif'
         }
 
-        if (['heif', 'avif'].includes(options.extension) == false) {
+        if (['jxl', 'avif'].includes(options.extension) == false) {
             options.extension = 'avif'
         }
 
-        const url = request.params['*']
-        const hash = Buffer.from(blake3(url)).toString('hex')
-    
-        // Handle the source file
-        
-            
-        if (await imageExistsByHash(hash) == false) {
-            if (pendingDownloads.has(hash)) {
-                return reply.status(408).send({ status: "The download is in progress!" })
-            }
+        const urlHash = Buffer.from(blake3(request.params['*'])).toString('hex')
 
-            await downloadFile(url, hash);
-        }
-
-        metadata = await metadataFile(hash);
-
-        if (isEmptyObject(metadata)) {
-            sourceImageSharp = sharp(path.join(IMAGEFOLDER, hash))
-            const sourceMetadata = await sourceImageSharp.metadata()
-            metadata['source'] = {
-                cid: 'TODO: CID',
-                hash: hash,
-                width: sourceMetadata.width,
-                height: sourceMetadata.height
-            }
-            metadata['variants'] = []
-        }
-
-        const returnData = await handleProcessing(metadata, options, sourceImageSharp)
-
-        if (returnData != null || isEmptyObject(returnData) == false) {
-            return returnData;
+        if (pendingDownloads.has(urlHash)) {
+            return reply.status(408).send({ status: "The download is in progress!" })
         } else {
-            return reply.status(408).send({ status: "The server has your image and tried to process it but failed." })
+            if (!await imageExists(urlHash)) {
+                pendingDownloads.add(urlHash)
+                await downloadFile(request.params['*'], urlHash);
+            }
+
+            const sourceImageSharp = sharp(path.join(IMAGEFOLDER, urlHash))
+            let resizeOptions = {}
+
+            if (options?.tallestSide) {
+                const sourceMetadata = sourceImageSharp.metadata();
+                if (sourceMetadata.width > sourceMetadata.height) {
+                    resizeOptions.width = options.tallestSide;
+                } else {
+                    resizeOptions.height = options.tallestSide;
+                }
+            }
+
+            // The destination file we're looking for is...
+            let destinationHash = `${Buffer.from(blake3(urlHash + JSON.stringify(request.body))).toString('hex')}`
+            let destinationFile = `${destinationHash}.${options.extension}`
+
+            if (await imageExists(destinationFile) == false) {
+                await sourceImageSharp.resize(resizeOptions).avif({ quality: 75 }).toFile(path.join(IMAGEFOLDER, destinationFile))
+                console.log(`created requested image with target filename: ${destinationFile}`)
+            } else {
+                console.log(`destination exists. Returned with filename: ${destinationFile}`)
+            }
+
+            return {hash: destinationHash, filename: destinationFile}
         }
-    
     } catch (error) {
         console.log(error)
         console.log(`The requested URL could not be parsed as image. URL: ${url}`)
